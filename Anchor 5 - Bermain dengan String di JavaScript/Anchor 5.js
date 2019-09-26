12beff2afecd9f7139b6cd09c2f89b2ea4de1c7f
japhendywijaya <jap_Hendy_wijaya@yahoo.co.id>
2019-09-26 14:46:00 +0700

    update lagi

--- Anchor 5 - Bermain dengan String di JavaScript/Anchor 5.js
+++ Anchor 5 - Bermain dengan String di JavaScript/Anchor 5.js
@@ -1,4 +1,5 @@
 // 1)
+console.log('nomor 1')
 var word = 'JavaScript';
 var second = 'is';
 var third = 'awesome';
@@ -10,5 +11,66 @@ var seventh = 'it!';
 console.log(word + " " + second + " " + third + " " + fourth + " " + fifth + " " + sixth + " " + seventh)
 console.log(word + second + third + fourth + fifth + sixth + seventh)
 
-
+console.log('\n','\n')
 // 2)
+console.log('nomor 2')
+
+var word = 'wow JavaScript is so cool';
+var exampleFirstWord = word[0] + word[1] + word[2];
+var secondWord = word[3] + word[4]  + word[5] + word[6] + word[7] + word[8] + word[9] + word[10] + word[11] + word[12] + word[13]
+var thirdWord = word[15] + word[16]
+var fourthWord = word[18] + word[19]
+var fifthWord =  word[21] + word[22] + word[23] + word[24]
+
+console.log('First Word: ' + exampleFirstWord);
+console.log('Second Word: ' + secondWord);
+console.log('Third Word: ' + thirdWord);
+console.log('Fourth Word: ' + fourthWord);
+console.log('Fifth Word: ' + fifthWord);
+
+
+console.log('\n','\n')
+// 3)
+console.log('nomor 3')
+
+var word = 'wow JavaScript is so cool';
+var exampleFirstWord3 = word.substring(0, 3);
+var secondWord3 = word.substring(4,14)
+var thirdWord3 = word.substring(15, 17)
+var fourthWord3 = word.substring(18, 20)
+var fifthWord3 = word.substring(21, 25)
+
+console.log('First Word: ' + exampleFirstWord3);
+console.log('Second Word: ' + secondWord3);
+console.log('Third Word: ' + thirdWord3);
+console.log('Fourth Word: ' + fourthWord3);
+console.log('Fifth Word: ' + fifthWord3);
+
+
+console.log('\n','\n')
+// 4)
+console.log('nomor 4')
+
+
+var word4 = 'wow JavaScript is so cool';
+var exampleFirstWord4 = word.substring(0, 3);
+var secondWord4 = word.substring(4,14)
+var thirdWord4 = word.substring(15, 17)
+var fourthWord4 = word.substring(18, 20)
+var fifthWord4 = word.substring(21, 25)
+
+var firstWordLength = exampleFirstWord4.length;
+var secondWord4Length = secondWord4.length
+var thirdWord4Length = thirdWord4.length
+var fourthWord4Length  = fourthWord4.length
+var fifthWord4Length = fifthWord4.length
+
+console.log('First Word: ' + exampleFirstWord + ', with length: ' + firstWordLength);
+console.log('Second Word: ' + secondWord + ', with length: ' + secondWord4Length);
+console.log('Third Word: ' + thirdWord + ', with length: ' + thirdWord4Length);
+console.log('Fourth Word: ' + fourthWord + ', with length: ' + fourthWord4Length);
+console.log('Fifth Word: ' + fifthWord + ', with length: ' + fifthWord4Length);
+
+
+
+
---
+++ Anchor 6 - Berlatih Perulangan di JavaScript/Anchor 6.js
@@ -0,0 +1,129 @@
+// nomor 1
+console.log('nomor 1')
+
+
+console.log('LOOPING PERTAMA')
+
+var x = 2
+
+while(x<=20){
+    console.log(x + ' - I love coding')
+    x += 2
+}
+
+
+console.log("LOOPING KEDUA")
+
+x = 20
+while(x>=2){
+    console.log(x + ' - I will become fullstack developer')
+    x -= 2
+}
+
+
+
+// nomor 2
+console.log("\n", "\n")
+console.log('nomor 2')
+
+console.log('LOOPING PERTAMA')
+
+var x = 1
+
+for(x; x<=20; x++){
+    console.log(x + ' - I Love Coding')
+}
+
+
+console.log("LOOPING KEDUA")
+
+x=20
+for(x; x>=1; x--){
+    console.log(x + ' - I will become fullstack developer')
+}
+
+ 
+
+ 
+// nomor 3
+console.log("\n", "\n")
+console.log('nomor 3')
+
+
+
+var counter = 1
+
+for(counter; counter<=100; counter++){
+    if ( counter%2 >0 ){
+        console.log(counter + ' adalah bilangan GANJIL')
+    }
+    else{
+        console.log(counter + ' adalah bilangan GENAP')
+    }
+}
+
+ 
+
+
+
+// nomor 3 - pertambahan 2 kelipatan 3
+
+console.log("\n", "\n")
+console.log('nomor 3 - pertambahan 2 kelipatan 3')
+
+var counter = 1
+
+for(counter; counter<=100; counter+=2){
+    
+    
+    if ( counter%3 == 0 ){
+        console.log(counter + ' adalah kelipatan 3')
+    }
+    else{
+    }
+
+
+}
+
+
+
+// nomor 3 - pertambahan 5 kelipatan 6
+
+console.log("\n", "\n")
+console.log('nomor 3 - pertambahan 5 kelipatan 6')
+
+var counter = 1
+
+for(counter; counter<=100; counter+=5){
+    
+    
+    if ( counter%6 == 0 ){
+        console.log(counter + ' adalah kelipatan 6')
+    }
+    else{
+    }
+
+
+}
+
+
+
+
+// nomor 3 - pertambahan 9 kelipatan 10
+
+console.log("\n", "\n")
+console.log('nomor 3 - pertambahan 9 kelipatan 10')
+
+var counter = 1
+
+for(counter; counter<=100; counter+=9){
+    
+    
+    if ( counter%10 == 0 ){
+        console.log(counter + ' adalah kelipatan 10')
+    }
+    else{
+    }
+
+
+}
